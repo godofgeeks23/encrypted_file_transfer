aba99df7e93511bb1c3a337cd8fde7f69891964f
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-02-10 22:02:52 +0530

    Update client.py

--- asymmetric_crypto/client.py
+++ asymmetric_crypto/client.py
@@ -4,10 +4,12 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import os
 
+
 def load_key_from_file(filename):
     with open(filename, 'rb') as f:
         key = f.read()
     return key
+
 
 def encrypt_file(file_path, public_key):
     with open(file_path, 'rb') as f:
@@ -18,11 +20,13 @@ def encrypt_file(file_path, public_key):
     encrypted_data = cipher.encrypt(data)
     return encrypted_data
 
+
 def decrypt_file(encrypted_data, private_key):
     rsa_key = RSA.import_key(private_key)
     cipher = PKCS1_OAEP.new(rsa_key)
     decrypted_data = cipher.decrypt(encrypted_data)
     return decrypted_data
+
 
 # Example usage
 if __name__ == "__main__":
