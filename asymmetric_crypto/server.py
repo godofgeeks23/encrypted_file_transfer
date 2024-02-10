a5d39c2f7c1317ec5afd10d783be24345c2a10fa
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-02-10 22:03:54 +0530

    Update server.py

--- asymmetric_crypto/server.py
+++ asymmetric_crypto/server.py
@@ -4,6 +4,7 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import os
 
+
 def load_key_from_file(filename):
     with open(filename, 'rb') as f:
         key = f.read()
@@ -31,7 +32,7 @@ if __name__ == "__main__":
 
     with open('encrypted_file.bin', 'rb') as f:
         encrypted_data = f.read()
-    
+
     server_private_key = load_key_from_file('server_private_key.pem')
 
     decrypted_data = decrypt_file(encrypted_data, server_private_key)
