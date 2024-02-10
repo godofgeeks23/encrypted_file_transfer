6b7b0be4b5914310578a62851d9ad7efb6f40156
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-02-10 22:03:50 +0530

    Update crypt.py

--- asymmetric_crypto/crypt.py
+++ asymmetric_crypto/crypt.py
@@ -2,20 +2,24 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import os
 
+
 def generate_key_pair():
     key = RSA.generate(2048)
     private_key = key.export_key()
     public_key = key.publickey().export_key()
     return private_key, public_key
 
+
 def save_key_to_file(key, filename):
     with open(filename, 'wb') as f:
         f.write(key)
+
 
 def load_key_from_file(filename):
     with open(filename, 'rb') as f:
         key = f.read()
     return key
+
 
 def encrypt_file(file_path, public_key):
     with open(file_path, 'rb') as f:
@@ -26,11 +30,13 @@ def encrypt_file(file_path, public_key):
     encrypted_data = cipher.encrypt(data)
     return encrypted_data
 
+
 def decrypt_file(encrypted_data, private_key):
     rsa_key = RSA.import_key(private_key)
     cipher = PKCS1_OAEP.new(rsa_key)
     decrypted_data = cipher.decrypt(encrypted_data)
     return decrypted_data
+
 
 # Example usage
 if __name__ == "__main__":
