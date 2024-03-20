a775835c26ad13e8361674160cf3404a1f89f013
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-03-20 22:57:38 +0530

    Update EasyCrypto.py

--- symmetric_crypto/EasyCrypto.py
+++ symmetric_crypto/EasyCrypto.py
@@ -5,17 +5,21 @@ import socket
 import time
 import sys
 
-#encrypt the data using the requested cipher and pad it to length
+# encrypt the data using the requested cipher and pad it to length
+
+
 def encryptAndPad(cipher, data):
-    padder = padding.PKCS7(128).padder()        #Pad the data as needed
+    padder = padding.PKCS7(128).padder()  # Pad the data as needed
     paddedData = padder.update(data) + padder.finalize()
     if cipher == None:
-            return paddedData
-    encryptor = cipher.encryptor()              #Encrypt the data and send
+        return paddedData
+    encryptor = cipher.encryptor()  # Encrypt the data and send
     ct = encryptor.update(paddedData) + encryptor.finalize()
     return ct
 
-#decrypt the data using the requested cipher and unpad it
+# decrypt the data using the requested cipher and unpad it
+
+
 def decryptAndUnpad(cipher, ct):
     if cipher != None:
         decryptor = cipher.decryptor()
@@ -24,44 +28,50 @@ def decryptAndUnpad(cipher, ct):
     pt = unpadder.update(ct) + unpadder.finalize()
     return pt
 
-#receive the file from the connection until it ends
+# receive the file from the connection until it ends
+
+
 def recieveFile(conn, cipher, filename):
-    file = open(filename,"wb")
+    file = open(filename, "wb")
     while True:
-        FlagAndChunkE = conn.recv(1024,socket.MSG_WAITALL)
+        FlagAndChunkE = conn.recv(1024, socket.MSG_WAITALL)
         if FlagAndChunkE != b'':
-                pt = decryptAndUnpad(cipher, FlagAndChunkE)
-                file.write(pt[1:])
-                file.flush()
-                if pt[0:1] == b'T':
-                    break
+            pt = decryptAndUnpad(cipher, FlagAndChunkE)
+            file.write(pt[1:])
+            file.flush()
+            if pt[0:1] == b'T':
+                break
     file.close()
     return True
 
-#send the entire file from server to client
+# send the entire file from server to client
+
+
 def sendFile(conn, cipher, filename):
-    file = open(filename,"rb")
+    file = open(filename, "rb")
     while True:
-        chunk = file.read(1022)                         #One byte for the flag, and one for padding
-        if len(chunk)==1022:                                #If its 1024 with no padding, it doesn't like it
+        chunk = file.read(1022)  # One byte for the flag, and one for padding
+        if len(chunk) == 1022:  # If its 1024 with no padding, it doesn't like it
             EOFAndChunk = b'F' + chunk
-            conn.send(encryptAndPad(cipher,EOFAndChunk))
+            conn.send(encryptAndPad(cipher, EOFAndChunk))
         else:
             EOFAndChunk = b'T' + chunk
-            conn.send(encryptAndPad(cipher,EOFAndChunk))
+            conn.send(encryptAndPad(cipher, EOFAndChunk))
             break
     file.close()
     return True
 
-#send the entire file from client to server
+# send the entire file from client to server
+
+
 def sendStdIn(conn, cipher, data):
     while True:
-        chunk = data.read(1022)                         #One byte for the flag, and one for padding
-        if len(chunk)==1022:                                #If its 1024 with no padding, it doesn't like it
+        chunk = data.read(1022)  # One byte for the flag, and one for padding
+        if len(chunk) == 1022:  # If its 1024 with no padding, it doesn't like it
             EOFAndChunk = b'F' + chunk
-            conn.send(encryptAndPad(cipher,EOFAndChunk))
+            conn.send(encryptAndPad(cipher, EOFAndChunk))
         else:
             EOFAndChunk = b'T' + chunk
-            conn.send(encryptAndPad(cipher,EOFAndChunk))
+            conn.send(encryptAndPad(cipher, EOFAndChunk))
             break
     return True
